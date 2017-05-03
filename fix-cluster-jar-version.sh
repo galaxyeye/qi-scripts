005ae22f37bf27e4128eaab217f3e862139d9d12
Vincent Zhang <ivincent.zhang@gmail.com>
2017-05-03 20:51:55 +0800

    Enhancement for sync-pulsar.sh

---
+++ fix-cluster-jar-version.sh
@@ -0,0 +1,10 @@
+#bin
+
+cp jsr305-3.0.0.jar /home/hduser/hadoop-2.5.2/share/hadoop/httpfs/tomcat/webapps/webhdfs/WEB-INF/lib/
+cp jsr305-3.0.0.jar /home/hduser/hadoop-2.5.2/share/hadoop/hdfs/lib/
+cp jsr305-3.0.0.jar /home/hduser/hadoop-2.5.2/share/hadoop/yarn/lib/
+cp jsr305-3.0.0.jar /home/hduser/hadoop-2.5.2/share/hadoop/tools/lib/
+cp jsr305-3.0.0.jar /home/hduser/hadoop-2.5.2/share/hadoop/common/lib/
+cp jsr305-3.0.0.jar /home/hduser/hbase-0.98.8/lib/
+
+
